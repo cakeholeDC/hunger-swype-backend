67b7362d6415cd12b3e35f97bbfc5905a366cad2
Kyle <kylepcole@gmail.com>
2020-01-29 14:41:02 -0500

    database seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  # resources :dish_courses
+  # resources :dish_cuisines
+  # resources :dish_diets
+  # resources :users
+  # resources :favorites
+  # resources :courses
+  # resources :cuisines
+  # resources :diets
+  resources :restaurants
+  resources :recipes
+  resources :dishes, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,164 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+DishDiet.destroy_all
+DishCuisine.destroy_all
+DishCourse.destroy_all
+Diet.destroy_all
+Cuisine.destroy_all
+Course.destroy_all
+Dish.destroy_all
+Recipe.destroy_all
+
+
+require_relative '../../api-data/apiDataToSeed.rb' ## imports API_DATA
+# require_relative '../API/spoonacular-100-random-vegan.json.rb'  ##imports VEGAN
+
+## IMPORT DIETS, CUISINE, and COURSES first
+diets = [
+	"vegetarian",
+	"vegan",
+	"gluten free",
+	"dairy free",
+	"fodmap friendly",
+	"whole 30",
+	"ketogenic",
+	"pescatarian",
+	"paleolithic"
+
+
+] 
+
+diets.each do |item|
+  Diet.create(name: item)
+end
+
+courses = [
+	"starter",
+	"appetizer",
+	"antipasti",
+	"antipasto",
+	"lunch",
+	"snack",
+	"main course",
+	"main dish",
+	"side dish",
+	"dinner",
+	"soup",
+	"seasoning",
+	"marinade"
+] 
+
+courses.each do |item|
+  Course.create(name: item)
+end
+
+cuisines = [
+	"Indian",
+	"Vietnamese",
+	"Asian",
+	"Mediterranean",
+	"Italian",
+	"Eastern European",
+	"European",
+	"Greek",
+	"French",
+	"Mexican",
+	"American",
+	"Spanish",
+	"South American",
+	"Latin American",
+	"Southern",
+	"German",
+	"Japanese",
+	"Thai"
+] 
+
+cuisines.each do |item|
+  Cuisine.create(name: item)
+end
+
+API_DATA[:recipes].each do |recipe|
+  #iterate through recipe.extendedIngredients and store as a string
+  ingredients = recipe[:extendedIngredients].map do |ingredient|
+    ingredient[:originalString]
+  end
+  #iterate through recipe.analyzedInstructions and reduce to the required data
+  if recipe[:analyzedInstructions].length > 0
+	  instructions = recipe[:analyzedInstructions][0][:steps].map do |instruction|
+	    {
+	      step: instruction[:number],
+	      details: instruction[:step]
+	    }
+	  end
+  else
+  	instructions = [{step: 1, details: recipe[:instructions]}]
+  end
+
+  importRecipe = Recipe.find_or_create_by(
+    api_id: recipe[:id],
+    title: recipe[:title],
+    photo: recipe[:image],
+    rating: recipe[:spoonacularScore],
+    servings: recipe[:servings],
+    cook_time: recipe[:readyInMinutes],
+    ingredients: ingredients.join('; '),
+  )
+
+  importRecipe.directions = instructions
+  importRecipe.save
+
+  dish = Dish.find_or_create_by(
+    api_id: recipe[:id],
+    photo: recipe[:image],
+    is_recipe: true,
+    recipe_id: importRecipe.id,
+    restaurant_id: nil
+  )
+
+  ## DIET RELATIONSHIPS
+  diets.each do |diet|
+  	if diet == "ketogenic"
+		recipe[diet.to_sym] == true
+  		DishDiet.create(
+  			dish_id: dish.id,
+  			diet_id: Diet.find_or_create_by(name: diet).id
+  		)
+	elsif recipe[:diets].include?(diet)
+		DishDiet.create(
+			dish_id: dish.id,
+			diet_id: Diet.find_or_create_by(name: diet).id
+		)
+	end
+  end
+
+  ## COURSE RELATIONSHIPS
+  courses.each do |course| 
+  	if recipe[:dishTypes].include?(course)  
+  		course = "antipasto" if course == "antipasti"
+  		course = "main dish" if course == "main course" 
+  		DishCourse.create(
+  			dish_id: dish.id,
+  			course_id: Course.find_or_create_by(name: course).id
+		)
+  		# dish.courses.build(name: Course.find_or_create_by(name: course))
+  	end
+  end
+
+  ## CUISINE RELATIONSHIPS
+  cuisines.each do |cuisine|
+  	if recipe[:cuisines].include?(cuisine)  
+  		DishCuisine.create(
+  			dish_id: dish.id,
+  			cuisine_id: Cuisine.find_or_create_by(name: cuisine).id
+  		)
+  		# dish.cuisines.build(name: Cuisine.find_or_create_by(name: cuisine))
+  	end
+  end
+
+  dish.save
+
+end
+
+
+
