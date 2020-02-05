6faed01089cf2e6cd755e47e7a0974fe9f69c8cb
Kyle <kylepcole@gmail.com>
2020-02-05 14:58:51 -0500

    login controllers

---
+++ app/controllers/api/ v1/auth_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::AuthController < ApplicationController
+
+	def create
+		userInstance = Musician.find_by(username: params[:username])
+
+		# isUserFound?
+		if userInstance
+			
+			# canUserBeAuthenticated?
+			if userInstance.authenticate(params[:password])
+				
+				# setUserToken
+				token = encode({ musician_id: userInstance.id })
+
+				# renderSerializedUserData
+				render json: {
+					currentUser: userInstance.to_json(
+				            except: [:updated_at, :created_at], 
+						),
+						jwt: token
+						}, status: :accepted
+			# userExistsWithoutAuth
+			else
+				render json: {
+					error: true,
+					message: "Incorrect Password"
+					}, status: :unauthorized
+				end
+		#userDoesNotExist
+		else
+			render json: {
+				error: true,
+				message: "Username not found"
+				}, status: :not_acceptable
+			end
+		end
+end
