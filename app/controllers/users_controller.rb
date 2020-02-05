fe17a6871e6075192a729861f353d2f0168edbae
Kyle <kylepcole@gmail.com>
2020-02-05 14:58:43 -0500

    login controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+
+	def create
+		byebug
+        user = User.create(
+            username: params[:username], 
+            password: params[:password], 
+            name: params[:name], 
+            region: params[:region], 
+            birthdate: params[:birthdate], 
+            avatar: params[:avatar], 
+        )
+
+        token = encode({ user_id: user.id })
+        render json: {
+            jwt: token,
+            currentUser: user.to_json(
+                except: [:updated_at, :created_at]
+            )
+        }
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(
+            username: params[:username], 
+            password: params[:password], 
+            name: params[:name], 
+            region: params[:region], 
+            birthdate: params[:birthdate], 
+            avatar: params[:avatar], 
+        )
+
+        token = encode({ user_id: user.id })
+        render json: {
+            jwt: token,
+            currentUser: user.to_json(
+                except: [:updated_at, :created_at]
+            )
+        }
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # resources :dish_courses
   # resources :dish_cuisines
   # resources :dish_diets
-  # resources :users
+  resources :users, only: [:create, :update]
   # resources :favorites
   resources :courses, only: [:index]
   resources :cuisines, only: [:index]
@@ -14,4 +14,11 @@ Rails.application.routes.draw do
 
   post '/get-matches/', to: 'dishes#match'
   get '/match/recipe/:id', to: 'recipes#show'
+
+  namespace :api do
+    namespace :v1 do
+      post '/login', to: 'dishes#show'#'auth#create'
+    end
+  end
+  
 end
