d180dd066ff3bcee35ddc4ebaa8e0136bed3dcb9
Kyle <kylepcole@gmail.com>
2020-03-04 14:16:21 -0500

    fixed zero result bug

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -5,48 +5,57 @@ class DishesController < ApplicationController
 		dishes = Dish.first
 
 		render json:dishes.to_json( include: :recipe)#[recipe: {only: [:id, :title, :rating, :servings, :cook_time, :photo]}])
-		
 	end
 
 	def match
-		#Diet.find_by(name: "vegan").dishes.select do |dish| dish.courses.map do |course| course.name end.include?("dinner") end.length
-
 		diets = params[:dietsFilter]
-		courses = params[:coursesFilter]
 		cuisines = params[:cuisinesFilter]
+		courses = params[:coursesFilter]
 
-		dishes = Dish.joins(:diets).where(diets: {name: diets}).uniq
-		
-		# newDishes = dishes
-		# filteredDishes = Array.new
-		dishCuisines = dishes.select do |dish| 
-			!(dish.get_cuisines & cuisines).empty? 
+		## IF NO DIETS ARE SELECTED
+		if diets.length < 1
+			## USE ALL DISHES
+			dishes = Dish.all
+		else
+			## ELSE USE DISHES THAT MATCH DIETS
+			dishes = Dish.joins(:diets).where(diets: {name: diets}).uniq
 		end
 
-		## FOR DEMO
-		# dishCourses = dishCuisines.select do |dish|
-		# 	!(dish.get_courses && courses).empty?
-		# end
+		## IF CUISINES ARE SELECTED
+		if cuisines.length > 0
+			## FILTER DIET RESULTS BY CUISINES
+			dishCuisines = dishes.select do |dish| 
+				!(dish.get_cuisines & cuisines).empty? 
+			end
+		else 
+			## ELSE USE DIET RESULTS
+			dishCuisines = dishes
+		end
 
-		## FOR DEMO
-		# while dishCourses.length < 50 do
-		while dishCuisines.length < 50 do
+		## IF COURSES ARE SELECTED
+		if courses.length > 0
+			## FILTER CUISINE RESULTS BY COURSES
+			dishCourses = dishCuisines.select do |dish|
+				!(dish.get_courses && courses).empty?
+			end
+		else
+			## ELSE USE CUISINE RESULTS
+			dishCourses = dishCuisines
+		end
+
+		## WHILE THERE ARE LESS THAN 50 RESULTS
+		while dishCourses.length < 50 do
 			
+			## PICK A RANDOM DISH (THAT MATCHES THE DIET PREFS)
 			add = dishes.sample
 			
-			## FOR DEMO
-			# if !dishCourses.include?(add) 
-			# 	dishCourses << add
-			# end
-
-			if !dishCuisines.include?(add) 
-				dishCuisines << add
+			## IF IT'S NOT A DUPLICATE, ADD TO RESULTS
+			if !dishCourses.include?(add) 
+				dishCourses << add
 			end
 
 		end
 
-		## FOR DEMO
-		# render json:dishCourses.to_json( include: [recipe: {only: [:id, :title, :rating, :servings, :cook_time, :photo]}])
-		render json:dishCuisines.to_json( include: [recipe: {only: [:id, :title, :rating, :servings, :cook_time, :photo]}])
+		render json:dishCourses.to_json( include: [recipe: {only: [:id, :title, :rating, :servings, :cook_time, :photo]}])
 	end
 end
